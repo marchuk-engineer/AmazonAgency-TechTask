903b192b528dbb4ca8011f00eaab11e943413f59
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-03-05 15:21:19 +0200

    Initial commit

---
+++ src/main/java/com/agencyamazon/AmazonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.agencyamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonTaskApplication.class, args);
+	}
+
+}
