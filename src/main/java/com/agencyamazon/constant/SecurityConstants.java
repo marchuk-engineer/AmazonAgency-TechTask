bcdcb0b7273f28d161ec7b8f76ae3705115ccb9c
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-03-06 00:35:56 +0200

    Security consts. CookieUtils to work with.

---
+++ src/main/java/com/agencyamazon/constant/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.agencyamazon.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SecurityConstants {
+
+    public static final String REFRESH_TOKEN = "refreshToken";
+
+    public static final String BEARER = "Bearer ";
+
+    public static final String USER_ID = "USER_ID";
+}
